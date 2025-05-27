16d9b3570c82d6c7cf65dcfef709dbf59313bd6b
Zeptiny <me@nyuu.dev>
2025-05-27 19:09:37 -0300

    Basic input

--- app/bin/app.dart
+++ app/bin/app.dart
@@ -1,9 +1,16 @@
-import 'models/entities.dart';
-void main(List<String> arguments) {
-  var hero = Hero("Carlitos", "Carlitos é brabo", 20, 0, "( ͡° ͜ʖ ͡°)");
-  var enemy = Enemy("Comum", "Carlitos", "Carlitos é brabo", 20, 0, "( ͡° ͜ʖ ͡°)");
+import 'dart:io';
 
-  print(hero.getName());
-  print(enemy.getType());
-  print(enemy.getHealth());
+import 'models/entities.dart';
+
+void main(List<String> arguments) {
+  print("Qual o nome do herói? ");
+  String? inName = stdin.readLineSync();
+  String name = inName ?? 'default';
+
+  print("Qual seria a história de $name?");
+  String? inDescription = stdin.readLineSync();
+  String description = inDescription ?? 'default';
+
+  // Level, vida e sprite padrão por enquanto
+  Hero hero = Hero(name, description, 20, 0, "( ͡° ͜ʖ ͡°)");
 }
