7617d21123758094a76f90eb3dd3493a580a33f8
Zeptiny <me@nyuu.dev>
2025-06-03 19:55:44 -0300

    chore: Autoformat from dart/flutter extension

--- app/bin/models/items.dart
+++ app/bin/models/items.dart
@@ -1,32 +1,39 @@
-abstract class Item{
+abstract class Item {
   String _name;
   String _description;
 
   Item(this._name, this._description);
 }
 
-class Consumable extends Item{
+class Consumable extends Item {
   int _effectStrength;
   String _effectType;
 
-  Consumable(String _name, String _description, this._effectStrength, this._effectType) : super(_name, _description);
+  Consumable(
+    String _name,
+    String _description,
+    this._effectStrength,
+    this._effectType,
+  ) : super(_name, _description);
 }
 
-
-class Weapon extends Item{
+class Weapon extends Item {
   int _damage;
   String _damageType;
 
-  Weapon(String _name, String _description, this._damage, this._damageType) : super(_name, _description);
+  Weapon(String _name, String _description, this._damage, this._damageType)
+    : super(_name, _description);
 }
 
-class Melee extends Weapon{
-// TO-DO
+class Melee extends Weapon {
+  // TO-DO
 
-  Melee(String _name, String _description, int _damage, String _damageType) : super(_name, _description, _damage, _damageType);
+  Melee(String _name, String _description, int _damage, String _damageType)
+    : super(_name, _description, _damage, _damageType);
 }
 
-class Ranged extends Weapon{
-// TO-DO
-  Ranged(String _name, String _description, int _damage, String _damageType) : super(_name, _description, _damage, _damageType);
+class Ranged extends Weapon {
+  // TO-DO
+  Ranged(String _name, String _description, int _damage, String _damageType)
+    : super(_name, _description, _damage, _damageType);
 }
--- app/bin/models/level.dart
+++ app/bin/models/level.dart
@@ -1,12 +1,11 @@
 import 'items.dart';
 import 'entities.dart';
 
-class Level{
+class Level {
   String _name;
   String _description;
   List<Enemy> _enemies;
   List<Item> _lootPool;
-
 
   get name => this._name;
   get description => this._description;
@@ -15,7 +14,5 @@ class Level{
   get enemyCount => this._enemies.length;
   get lootPoolSize => this._lootPool.length;
 
-
   Level(this._name, this._description, this._enemies, this._lootPool);
 }
-
